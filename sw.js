799fda13a08775d9b8212b0c739959b1e47b88ae
nvrqt03 <ajktg21@gmail.com>
2019-02-17 16:19:05 -0600

    resovled bug in sw file

--- sw.js
+++ sw.js
@@ -1,27 +1,29 @@
+
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
 self.addEventListener('install', function(event) {
-    event.waitUntil (
+    event.waitUntil(
         caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                'index.html',
-                '/restaurant.html',
-                '/css/styles.css',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/data/restaurants.json',
-                '/img/1.jpg',
-                '/img/2.jpg',
-                '/img/3.jpg',
-                '/img/3.jpg',
-                '/img/4.jpg',
-                '/img/5.jpg',
-                '/img/6.jpg',
-                '/img/7.jpg',
-                '/img/8.jpg',
-                '/img/9.jpg',
-                '/img/10.jpg'
-            ]);
+            return cache.addAll(cacheFiles);
         })
     );
 });
